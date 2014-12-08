af2817986da8212862747789122100db7ef0fc20
takuti <k.takuti@gmail.com>
2014-12-08 10:55:40 +0900

    solve p009

---
+++ set01/p009.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+from operator import itemgetter
+
+lines = map(lambda l: l.rstrip('\n').split('\t'), open('../data/address.txt').readlines())
+
+for l in sorted(lines, key=itemgetter(1,0), reverse=True):
+  print '%s\t%s' % (l[0], l[1])
+
