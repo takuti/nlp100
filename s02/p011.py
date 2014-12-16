47fada954a0cfece4d87d09b868453263beec43b
takuti <k.takuti@gmail.com>
2014-12-17 08:36:41 +0900

    add pre-compile of RE

--- s02/p011.py
+++ s02/p011.py
@@ -4,5 +4,6 @@ import re
 
 tweets = map(lambda l: l.rstrip('\n'), open('../data/tweets.txt').readlines())
 
+r = re.compile(u'拡散希望'.encode('utf-8'))
 for t in tweets:
-  if re.search(u'拡散希望'.encode('utf-8'), t): print t
+  if r.search(t): print t
